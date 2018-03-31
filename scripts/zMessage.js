d8ad5ee0d3522306a9f191dee20dd6c7db2b6e21
Hugo Saldana <hugo.saldanagn@gmail.com>
2018-03-31 18:42:57 -0400

    ok

--- scripts/zMessage.js
+++ scripts/zMessage.js
@@ -1,24 +1,35 @@
 var zMessage = zMessage || (function ($) {
     'use strict';
-    var functionzMessage;
+    var functionzMessageOk;
+    var functionzMessageCancel;
     
-    // Creating modal dialog's DOM
     var $dialog = $(
-        '<div class="modal fade" data-backdrop="static" data-keyboard="false" tabindex="-1" role="dialog" aria-hidden="true" style="padding-top:15%; overflow-y:visible;">' +
-        '<div class="modal-dialog modal-m">' +
-        '<div class="modal-content">' +
-        '<div class="modal-header"><h4 style="margin:0;"></h4></div>' +
-        '<div class="modal-body">' +
-        '<div><table><tbody><tr><td style="width: 50px;"><img id="image"/></td>' + 
-        '<td id="message" style="width: 600px; text-align: center;"></td>' +
-        '<td style="width: 50px;"></td></tr><tr><td colspan="3" style="height: 10px;"></td></tr>' +
-        '<tr><td style="text-align: center;" colspan="3">' +
-        '<button id="btnOk" style="width: 110px;" onclick="zMessage.hide(this);">Aceptar</button> ' +
-        '<button id="btnCancel"style="width: 110px;" onclick="zMessage.hide(this);">Cancelar</button>' +
-        '</td></tr></tbody></table></div>' +
-        '</div></div></div></div>');
+        '<div class="modal zmodal fade" data-backdrop="static" data-keyboard="false" tabindex="-1" role="dialog" aria-hidden="true">' +
+            '<div class="modal-dialog modal-m" role="document">' +
+                '<div class="modal-content">' +
+                    '<div class="modal-header">'+
+                        '<h4 class="zh4"></h4>'+
+                    '</div>' +
+                    '<div class="modal-body">' +
+                        '<div class="row">'+
+                            '<div id="row-image">'+
+                                '<img id="image"/>' + 
+                            '</div>'+
+                            '<div id="row-message">'+
+                                '<p id="message"></p>' + 
+                            '</div>'+
+                        '</div>'+  
+                    '</div>'+
+                '<div class="modal-footer">' + 
+                    '<button id="btnOk" onclick="zMessage.hide(this);">Aceptar</button> ' +
+                    '<button id="btnCancel" onclick="zMessage.hide(this);">Cancelar</button>' +  
+                '</div>' +
+            '</div>' +
+        '</div>'
+    );
 
     return {
+
 		/**
 		 * Opens our dialog
          * @param title   Custom Title
@@ -28,8 +39,11 @@ var zMessage = zMessage || (function ($) {
 		 * 				  options.dialogSize - bootstrap postfix for dialog size, e.g. "sm", "m";
 		 * 				  options.buttonType - bootstrap postfix for button class type, e.g. "success", "warning".
 		 */
+
         show: function (title, message, icon, options) {
+
             // Assigning defaults
+
             $dialog.find('.modal-header').show();
             if (typeof title === 'undefined' || title === '') {
                 $dialog.find('.modal-header').hide();
@@ -41,24 +55,42 @@ var zMessage = zMessage || (function ($) {
                 options = {};
             }
 
-            if (typeof message === 'undefined') {
+            if (typeof message === 'undefined' || message === '') {
                 message = '¿Está seguro de proceder?';
+                $dialog.find('#message').html(message);
             }
             $dialog.find('#message').html(message);
 
-            if (typeof icon === 'undefined'){
+            if (typeof icon === 'undefined' || icon === 0){
                 $dialog.find('#image').attr('src','');
+                $dialog.find('#row-image').hide();
+                $dialog.find('#row-message').attr('class', 'col-md-12 text-center');
             }
             else{
                 switch(icon){
-                    case 1: $dialog.find('#image').attr('src','content/img/ico_1.png');
+                    case 1: $dialog.find('#image').attr('src','content/img/1.png');
                             break;
-                    case 2: $dialog.find('#image').attr('src','content/img/ico_2.png');
+                    case 2: $dialog.find('#image').attr('src','content/img/2.png');
                             break;
-                    case 3: $dialog.find('#image').attr('src','content/img/ico_3.png');
+                    case 3: $dialog.find('#image').attr('src','content/img/3.png');
+                            break;
+                    case 4: $dialog.find('#image').attr('src','content/img/4.png');
+                            break;
+                    case 5: $dialog.find('#image').attr('src','content/img/5.png');
+                            break;
+                    case 6: $dialog.find('#image').attr('src','content/img/6.png');
+                            break;
+                    case 7: $dialog.find('#image').attr('src','content/img/7.png');
+                            break;
+                    case 8: $dialog.find('#image').attr('src','content/img/8.png');
+                            break;
+                    case 9: $dialog.find('#image').attr('src','content/img/9.png');
                             break;
                     default: $dialog.find('#image').attr('src','');
                 }
+                $dialog.find('#row-image').show();
+                $dialog.find('#row-image').attr('class', 'col-md-2 text-center');
+                $dialog.find('#row-message').attr('class', 'col-md-10 text-left');
             }
 
             var settings = $.extend({
@@ -66,36 +98,58 @@ var zMessage = zMessage || (function ($) {
                 buttonType: 'btn-danger',
                 buttonOK: true,
                 buttonCancel: true,
-                onClickOK: null // This callback runs after the dialog was hidden
+                onClickOK: null,
+                onClickCancel: null
             }, options);
 
+            if(!(typeof settings.buttonType === 'undefined' || settings.buttonType === '')){
+                switch(settings.buttonType){
+                    case 'btn-primary': $dialog.find('.modal-header').attr('class', 'modal-header modal-header-primary');
+                    break;
+                    case 'btn-success': $dialog.find('.modal-header').attr('class', 'modal-header modal-header-success');
+                    break;
+                    case 'btn-info': $dialog.find('.modal-header').attr('class', 'modal-header modal-header-info');
+                    break;
+                    case 'btn-warning': $dialog.find('.modal-header').attr('class', 'modal-header modal-header-warning');
+                    break;
+                    case 'btn-danger': $dialog.find('.modal-header').attr('class', 'modal-header modal-header-danger');
+                    break;
+                    case 'btn-link': $dialog.find('.modal-header').attr('class', 'modal-header');
+                    break;
+                    case 'btn-default': $dialog.find('.modal-header').attr('class', 'modal-header');
+                    break;
+                    default: $dialog.find('.modal-header').attr('class', 'modal-header '+ settings.buttonType);
+                }
+            }
 
             // Configuring dialog
+
             $dialog.find('.modal-dialog').attr('class', 'modal-dialog').addClass('modal-' + settings.dialogSize);
-            $dialog.find('button').attr('class', 'button '+ settings.buttonType);
+            $dialog.find('button').attr('class', 'button zbutton '+ settings.buttonType);
             
             $dialog.find('#btnOk').show();
             if (typeof settings.buttonOK === 'undefined' || settings.buttonOK === false) {
                 $dialog.find('#btnOk').hide();
             }
-            functionzMessage = settings.onClickOK;
-
+            functionzMessageOk = settings.onClickOK;
+            
             $dialog.find('#btnCancel').show();
             if (typeof settings.buttonCancel === 'undefined' || settings.buttonCancel === false) {
                 $dialog.find('#btnCancel').hide();
             }
+            functionzMessageCancel = settings.onClickCancel;
 
-            // Opening dialog
             $dialog.modal();
         },
-		/**
-		 * Closes dialog
-		 */
         hide: function (b) {
             $dialog.modal('hide');
             if (b.id === 'btnOk'){
-                if (typeof functionzMessage === 'function') {
-                    functionzMessage();
+                if (typeof functionzMessageOk === 'function') {
+                    functionzMessageOk();
+                }
+            } else if (b.id === 'btnCancel'){
+                if (typeof functionzMessageCancel === 'function') {
+                    functionzMessageCancel();
                 }
             }            
         }
